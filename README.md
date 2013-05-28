93d466f3c9fbafdf7c5ba36b42d887f085a858e4
websanova <rob@websanova.com>
2013-05-28 10:18:55 +0700

    Initial commit of wInput v1.0.0

---
+++ README.md
@@ -0,0 +1,59 @@
+# wInput.js
+
+A small jQuery plugin for `textarea`, `password` and `text` inputs.  This plugin was designed to take care of nagging issues for inputs like backwards compatability with placeholders for unsupported browsers (including password fields), labels and styling.  Since we are already applying the plugin for placeholders we might as well apply some other things as well.
+
+[View the wInput demo here](http://winput.websanova.com)
+
+## Related Plugins
+
+* [wSelect](http://wselect.websanova.com) - Custom select boxes.
+* [wCheck](http://wcheck.websanova.com) - Radio and checkbox input plugin.
+* [wChar](http://wchar.websanova.com) - On the fly character counter for inputs.
+
+
+## Settings
+
+Available options with notes, the values here are the defaults.
+
+```js
+$.fn.wInput.defaults = {
+    theme: 'classic',        // set theme for inputs
+    labelPosition: 'left',   // set position for label (left,top)
+    highlight: true          // highlight field when selected
+};
+```
+
+## Examples
+
+For starters you will need to include the following files:
+
+```js
+<script type="text/javascript" src="./wInput.js"></script>
+<link rel="Stylesheet" type="text/css" href="./wInput.css" />
+```
+
+You can then apply the plugin to any input text, password and textarea elements using the jQuery selector function:
+
+```js
+$('input:text, input:password, textarea').wInput();
+```
+
+You can also optionally set the `data-label` attribute and a label will automatically be created for you.  It's position will be beased on the `labelPosition` option.
+
+```html
+<input type="text" data-label="Username:" placeholder="Enter a username"/>
+```
+
+
+## Resources
+
+* [More jQuery plugins by Websanova](http://websanova.com/plugins)
+* [jQuery Plugin Development Boilerplate](http://www.websanova.com/tutorials/jquery/jquery-plugin-development-boilerplate)
+* [The Ultimate Guide to Writing jQuery Plugins](http://www.websanova.com/tutorials/jquery/the-ultimate-guide-to-writing-jquery-plugins)
+
+
+## License
+
+MIT licensed
+
+Copyright (C) 2011-2013 Websanova http://www.websanova.com
---
+++ wInput.js
@@ -0,0 +1,248 @@
+/******************************************
+ * Websanova.com
+ *
+ * Resources for web entrepreneurs
+ *
+ * @author          Websanova
+ * @copyright       Copyright (c) 2012 Websanova.
+ * @license         This websanova wInput jQuery plug-in is dual licensed under the MIT and GPL licenses.
+ * @link            http://www.websanova.com
+ * @github          http://github.com/websanova/wInput
+ * @version         Version 1.0.0
+ *
+ ******************************************/
+
+(function($) {
+    
+    function Input(el, options) {
+        this.$el = $(el);
+        this.options = options;
+        this.phState = true;
+        this.type = this.$el.prop('type').toLowerCase();
+
+        this.generate();
+    };
+    
+    Input.prototype = {
+        generate: function() {
+            var _self = this;
+            
+            this.$el.addClass('wInput wInput-' + this.$el.prop('tagName').toLowerCase());
+            this.createLabel();
+            this.setTheme(this.options.theme);
+
+            if (!$.support.placeholder) {
+                this.togglePassword();
+
+                this.$el.keydown(function(e){ _self.onKeydown(e); });
+                this.$el.keyup(function(e){ _self.onKeyup(e); });
+                this.$el.mousedown(function(e){ _self.onMousedown(e); });
+
+                $(document).ready(function() {
+                    var text = _self.$el.attr('placeholder');
+
+                    if (_self.$el.val() === '' || _self.$el.val() === _self.$el.attr('placeholder')) {
+                        _self.$el.val(text);
+                        _self.$el.addClass('wInput-placeholder');
+                    }
+                    else if (_self.$el.val() !== text) {
+                        _self.phState = false;
+                    }
+                });
+            }
+
+            this.$el.focus(function(){ _self.onFocus(); });
+            this.$el.blur(function(){ _self.onBlur(); });
+
+            return this.$el;
+        },
+
+        createLabel: function() {
+            var text = this.$el.attr('data-label');
+
+            if (text && text !== '') {
+                var id = this.$el.attr('id') || Math.random() * 100;
+
+                this.$el.attr('id', id);
+                this.$label = $('<label for="' + id + '" class="wLabel wLabel-theme-' + this.options.theme + ' wLabel-' + this.$el.prop('tagName').toLowerCase() + ' wLabel-' + this.options.labelPosition + '"></label>');
+
+                this.setLabel(text);
+                this.$el.before(this.$label);
+            }
+        },
+
+        togglePassword: function() {
+            if (this.type === 'password') {
+                var _self = this,
+                    $clone = this.$el.clone().wrap('<span></span>').parent('span').html();
+
+                if (this.phState === true) {
+                    $clone = $($clone.replace(/type=\"?password\"?/, 'type="text"'));
+                }
+                else {
+                    if ($clone.search(/type=\"?text\"?/) > -1) {
+                        $clone = $($clone.replace(/type=\"?text\"?/, 'type="password"'));
+                    }
+                    else {
+                        $clone = $($clone.replace(/>/, 'type="password">'));
+                    }
+                }
+
+                $clone.blur(function(){ _self.onBlur(); });
+                $clone.focus(function(){ _self.onFocus(); });
+                $clone.mousedown(function(e){ _self.onMousedown(e); });
+                $clone.keydown(function(e){ _self.onKeydown(e); });
+                $clone.keyup(function(e){ _self.onKeyup(e); });
+
+                this.$el.replaceWith($clone);
+                this.$el = $clone;
+            }
+        },
+
+        onMousedown: function() {
+            if (this.phState === true) {
+                this.$el.blur().focus();
+            }
+        },
+
+        onFocus: function() {
+            if (!$.support.placeholder && this.phState === true) {
+                this.setInputPosition(0);
+            }
+
+            if (this.options.highlight) {
+                if (this.$label) {
+                    this.$label.addClass('wLabel-active');
+                }
+                this.$el.addClass('wInput-active');
+            }
+        },
+
+        onBlur: function() {
+            if (this.options.highlight) {
+                if (this.$label) {
+                    this.$label.removeClass('wLabel-active');
+                }
+                this.$el.removeClass('wInput-active');
+            }
+        },
+
+        onKeydown: function(e) {
+            if (this.phState === true) {
+                this.phState = false;
+                this.$el.val('');
+                this.$el.removeClass('wInput-placeholder');
+            }
+
+            // special case for tabs which will not toggle keyup so we do it manually to trigger proper state
+            if((e.keyCode || e.which) === 9) {
+                this.onKeyup(e);
+            }
+        },
+
+        onKeyup: function(e) {
+            if (this.$el.val() === '') {
+                this.phState = true;
+
+                // special case for tabs here as well, don't need to toggle password in this situation, it will already be toggled in proper position at this point
+                if (this.type === 'password' && (e.keyCode || e.which) !== 9) {
+                    this.togglePassword();
+                    this.setInputPosition(0);
+                }
+
+                this.$el.val(this.$el.attr('placeholder'));
+                this.$el.addClass('wInput-placeholder');
+                this.onMousedown();
+            }
+            else if (this.type === 'password' && this.$el.prop('type').toLowerCase() === 'text') {
+                var val = this.$el.val();
+
+                this.togglePassword();
+                this.$el.val(val); // keep this line separate and not below, otherwise typing too fast will put the caret in wrong position
+                this.setInputPosition(this.$el.val().length);
+            }
+        },
+
+        setInputPosition: function(index) {
+            var input = this.$el[0];
+
+            if (input.setSelectionRange) {
+                input.setSelectionRange(index, index);
+            } else if (input.createTextRange) {
+                var range = input.createTextRange();
+                range.collapse(true);
+                range.moveEnd('character', index);
+                range.moveStart('character', index);
+                range.select();
+            }
+        },
+
+        setTheme: function(theme) {
+            this.$el.attr('class', this.$el.attr('class').replace(/wInput-theme-.+\s|wInput-theme-.+$/, ''));
+            this.$el.addClass('wInput-theme-' + theme);
+        },
+
+        setLabel: function(text) {
+            this.$label.html(text);
+        }
+    };
+    
+    $.support.placeholder = 'placeholder' in document.createElement('input');
+
+    $.fn.wInput = function(options, value) {
+        if (typeof options === 'string') {
+            var values = [];
+            var elements = this.each(function() {
+                var wInput = $(this).data('wInput');
+
+                if (wInput) {
+                    var func = (value ? 'set' : 'get') + options.charAt(0).toUpperCase() + options.substring(1).toLowerCase();
+
+                    if (wInput[options]) {
+                        wInput[options].apply(wInput, [value]);
+                    } else if (value) {
+                        if (wInput[func]) { wInput[func].apply(wInput, [value]); }
+                        if (wInput.options[options]) { wInput.options[options] = value; }
+                    } else {
+                        if(wInput[func]) { values.push(wInput[func].apply(wInput, [value])); }
+                        else if (wInput.options[options]) { values.push(wInput.options[options]); }
+                        else { values.push(null); }
+                    }
+                }
+            });
+
+            if (values.length === 1) { return values[0]; }
+            else if (values.length > 0) { return values; }
+            else { return elements; }
+        }
+
+        options = $.extend({}, $.fn.wInput.defaults, options);
+
+        function get(el) {
+            var wInput = $.data(el, 'wInput');
+            if (!wInput) {
+                var _options = jQuery.extend(true, {}, options);
+                wInput = new Input(el, _options);
+                $.data(el, 'wInput', wInput);
+            }
+
+            return wInput;
+        }
+
+        return this.each(function() {
+            var tagName = $(this).prop('tagName').toLowerCase(),
+                inputType = ($(this).prop('type') || '').toLowerCase();
+
+            if (tagName === 'textarea' || (tagName === 'input' && (inputType === 'text' || inputType === 'password') ) ) {
+                get(this);
+            }
+        });
+    };
+    
+    $.fn.wInput.defaults = {
+        theme: 'classic',        // set theme for inputs
+        labelPosition: 'left',   // set position for label (left,top)
+        highlight: true          // highlight field when selected
+    };
+    
+})(jQuery);
